209c26e7e38774296bac883d6805c3d1870b2e3e
Kamil Kurys <kkurys@10g.pl>
2018-12-01 14:09:50 +0100

    created schedule viewmodels

---
+++ src/Allergo.Web/ViewModels/Appoinment/AppointmentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Allergo.Web.ViewModels.Appoinment
+{
+    public class AppointmentViewModel
+    {
+        public int Hour { get; set; }
+        public int Minutes { get; set; }
+    }
+}
---
+++ src/Allergo.Web/ViewModels/Appoinment/CancelAppointmentViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Allergo.Web.ViewModels.Appoinment
+{
+    public class CancelAppointmentViewModel
+    {
+        public string AppointmentId { get; set; }
+    }
+}
