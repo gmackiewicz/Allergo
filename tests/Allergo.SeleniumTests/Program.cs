46355a45490ca96cee6fd6364a2436afc5609a1c
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2018-12-01 18:18:04 +0100

    added selenium tests proj

---
+++ tests/Allergo.SeleniumTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Allergo.SeleniumTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Manage().Window.Maximize();
+            }
+        }
+    }
+}
