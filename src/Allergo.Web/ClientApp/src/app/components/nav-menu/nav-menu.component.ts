407448502713c93a65ccb8890d7e10c45b6347be
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2018-12-15 21:35:07 +0100

    fixed logout

--- src/Allergo.Web/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ src/Allergo.Web/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-nav-menu',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+    constructor(private router: Router) {
+    }
+
     isExpanded = false;
 
     collapse() {
@@ -18,6 +22,6 @@ export class NavMenuComponent {
 
     logout() {
         localStorage.clear();
-        window.location.reload();
+        this.router.navigateByUrl('/login');
     }
 }
