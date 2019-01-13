113203d468e61abb5feccdc713f35f569f343ae7
Kamil Kurys <kkurys@10g.pl>
2019-01-13 01:31:53 +0100

    further work on homepage dashboard

--- src/Allergo.Web/ClientApp/src/app/components/home/home.component.ts
+++ src/Allergo.Web/ClientApp/src/app/components/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { HomeService } from '../../services/home.service';
+import { DashboardData } from '../../models/dashboard-data.model';
+import { Statistics } from '../../models/statistics.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
     constructor(private homeService: HomeService) { }
-
+    dashboardData: DashboardData = new DashboardData(new Statistics(0, 0, 0));
     ngOnInit() {
-        this.homeService.getDashboardData().subscribe(result => console.log(result));
+        this.homeService
+            .getDashboardData()
+            .subscribe(result => this.dashboardData = result);
     }
 }
