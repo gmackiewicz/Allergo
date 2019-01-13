c18d5b6b1c719f02626cfd2a1417e8971c782495
Kamil Kurys <kkurys@10g.pl>
2019-01-13 14:02:12 +0100

    added different dashboard views for admin and doctor

--- src/Allergo.Web/ClientApp/src/app/components/home/home.component.ts
+++ src/Allergo.Web/ClientApp/src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HomeService } from '../../services/home.service';
 import { DashboardData } from '../../models/dashboard-data.model';
 import { Statistics } from '../../models/statistics.model';
+import { RolesUtil } from '../../utils/roles.util';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import { Statistics } from '../../models/statistics.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    constructor(private homeService: HomeService) { }
+    constructor(private homeService: HomeService, private rolesUtil: RolesUtil) { }
     dashboardData: DashboardData = new DashboardData(new Statistics(0, 0, 0));
     ngOnInit() {
         this.homeService
